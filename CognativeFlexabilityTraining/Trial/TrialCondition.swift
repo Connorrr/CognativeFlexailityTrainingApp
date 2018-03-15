4a2e95c68064aa1a86729a3be655f60a3a1ad749
Connorrr <connor.reid87@gmail.com>
2018-03-15 13:43:46 +1000

    first commit

---
+++ CognativeFlexabilityTraining/Trial/TrialCondition.swift
@@ -0,0 +1,14 @@
+//
+//  TrialCondition.swift
+//  CognativeFlexabilityTraining
+//
+//  Created by Connor Reid on 15/3/18.
+//  Copyright © 2018 Connor Reid. All rights reserved.
+//
+
+enum TrialCondition {
+    case even
+    case odd
+    case above
+    case below
+}
---
+++ CognativeFlexabilityTraining/Trial/TrialInfo.swift
@@ -0,0 +1,13 @@
+//
+//  TrialInfo.swift
+//  CognativeFlexabilityTraining
+//
+//  Created by Connor Reid on 15/3/18.
+//  Copyright © 2018 Connor Reid. All rights reserved.
+//
+
+struct TrialInfo {
+    var question : String!
+    var condition : TrialCondition!
+    var correctResponse : TrialCondition!
+}
