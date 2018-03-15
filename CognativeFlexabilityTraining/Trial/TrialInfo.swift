268d9aa84fd1d01273d52b7779c3909c0aee6216
Connorrr <connor.reid87@gmail.com>
2018-03-15 16:33:04 +1000

    Added Stimulus images

--- CognativeFlexabilityTraining/Trial/TrialInfo.swift
+++ CognativeFlexabilityTraining/Trial/TrialInfo.swift
@@ -5,8 +5,11 @@
 //  Created by Connor Reid on 15/3/18.
 //  Copyright © 2018 Connor Reid. All rights reserved.
 //
+import UIKit
 
 struct TrialInfo {
     var question : String?
     var condition : TrialCondition?
+    var correctResponse : TrialCondition?
+    var stim : UIImage?
 }
