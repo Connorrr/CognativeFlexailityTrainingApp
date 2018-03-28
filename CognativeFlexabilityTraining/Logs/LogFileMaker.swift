985638b426fc1f98e9e650ebc970a66c526bcd43
Connorrr <connor.reid87@gmail.com>
2018-03-28 16:02:57 +1000

    Added function to the LogFileMaker to cover the experiment data to a csv string

--- CognativeFlexabilityTraining/Logs/LogFileMaker.swift
+++ CognativeFlexabilityTraining/Logs/LogFileMaker.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class LogFileMaker {
+    var fName: String
+    
+    init(fileName: String) {
+        fName = fileName
+    }
+    
+    func convertLogFileDataToCSVString() -> String {
+        var csvString = "Block No, Trial No, Trial Type, Trial Condition, Stim, Response, Rt, Correct, Time Elapsed\n"
+        
+        for blockData in LogFileData.logData {
+            for trialData in blockData.trialDataArray {
+                csvString.append("\(String(describing: trialData.blockNumber)),\(String(describing: trialData.trialNum)),\(String(describing: trialData.trialType)),\(String(describing: trialData.trialCondition)),\(String(describing: trialData.stim)),\(String(describing: trialData.response)),\(String(describing: trialData.rt)),\(String(describing: trialData.corr)),\(String(describing: trialData.time))\n")
+            }
+        }
+        
+        return csvString
+    }
+}
